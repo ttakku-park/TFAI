8e07c7d204435e41fd2ceb668280c0e2d6f4b42a
ttakku-park <ttakku.park@gmail.com>
2021-08-30 20:54:41 +0900

    feat | 값반환 로직 간소화 수정

--- InventoryAllocator.py
+++ InventoryAllocator.py
@@ -10,7 +10,6 @@ class InventoryAllocator :
             warehouse_fruit = list(warehouse_inven.keys())
             
             for i in warehouse_fruit :
-
                 if i in warehouse_count :
                     warehouse_count[i] +=  warehouse_inven.get(i)
                 else :
@@ -20,6 +19,7 @@ class InventoryAllocator :
         
         # 창고수용합계와 주문수량 검사 | 1. 창고수용량 < 주문량 -> return [] 2. 주문과일 재적창고 미존재시 return [] 
         for fruit in order_fruit :
+            
             if fruit in warehouse_count :
                 if order[fruit] > warehouse_count[fruit] :
                     return f"창고에 {fruit}를 저장할 수 없습니다.(창고수용량보다 주문수가 많습니다.)"
@@ -28,29 +28,21 @@ class InventoryAllocator :
 
         warehouse_dict = {}
         for section in warehouse :
-
             warehouse_name  = section["name"]
             warehouse_inven = section["inventory"]
 
-            for fruit in order_fruit : # [apple,banana]
+            for fruit in order_fruit : 
+
+                if not warehouse_name in warehouse_dict :
+                    warehouse_dict[warehouse_name] = {}
 
                 if warehouse_inven[fruit] >= order[fruit] :
-                    if not warehouse_name in warehouse_dict :
-                        warehouse_dict[warehouse_name] = {}
-                        warehouse_dict[warehouse_name][fruit] = order[fruit]
-                    else :
-                        warehouse_dict[warehouse_name][fruit] = order[fruit]
+                    warehouse_dict[warehouse_name][fruit] = order[fruit]
+                    order[fruit] = 0
 
-                if warehouse_inven[fruit] < order[fruit] :
-                    if not warehouse_name in warehouse_dict :
-                        warehouse_dict[warehouse_name] = {}
-                        warehouse_dict[warehouse_name][fruit] = warehouse_inven[fruit]
-                        order[fruit] -= warehouse_inven[fruit]
-
-                    else :
-                        warehouse_dict[warehouse_name][fruit] = warehouse_inven[fruit]
-                        order[fruit] -= warehouse_inven[fruit]
-
+                elif warehouse_inven[fruit] < order[fruit] :
+                    warehouse_dict[warehouse_name][fruit] = warehouse_inven[fruit]
+                    order[fruit] -= warehouse_inven[fruit]
                 
         return [warehouse_dict]
 
@@ -58,7 +50,7 @@ class InventoryAllocator :
 
 
     order = { "apple":12 , "banana":1}
-    warehouse = [{ "name": "owd", "inventory": { "apple":4 ,"banana":0}},{ "name": "dm", "inventory": { "apple":6,"banana":1}}]
+    warehouse = [{ "name": "owd", "inventory": { "apple":15,"banana":1}},{ "name": "dm", "inventory": { "apple":6,"banana":1}}]
 
     print(inventory_forwarding(order, warehouse))
 
